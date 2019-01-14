a08133207147f3995a9b3f9fd2e7836279849c9c
Arnor Ameti <arnorameti.ch@gmail.com>
2019-01-14 10:21:41 +0100

    Abgabe_2

---
+++ hashmap.py
@@ -0,0 +1,28 @@
+class HashMap:
+ def __init__(self, array_size):
+  self.array_size = array_size
+  self.array = [None for item in range(array_size)]
+
+ def hash(self, key):
+  key_bytes = key.encode()
+  hash_code = sum(key_bytes)
+  return hash_code
+
+  def compressor(self, hash_code):
+   return hash_code % self.array_size
+
+  def assign(self, key, value):
+   array_index = self.compressor(self.hash(key))
+   self.array[array_index] = value
+
+  def retrieve(self, key):
+   array_index = self.compressor(self.hash(key))
+   return self.array[array_index]
+
+   # hash_map = HashMap(50)
+   # hash_map.assign('y', 'yes')
+   # hash_map.assign('n', 'no')
+   # hash_map.assign('g', 'german')
+   # hash_map.assign('j', 'japenese')
+   # hash_map.assign('v', 'vegetarian')
+   # hash_map.assign('f', 'french')
